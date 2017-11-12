12e0e7559ac30e64ecb9c1d5256ee6471ddf8d30
Amnart Saejueng <tootop101@hotmail.com>
2017-11-12 12:43:20 +0700

    Add build.sbt to SparkMapPartitionsWithInputSplitExample.

--- SparkMapPartitionsWithInputSplitExample/src/main/scala/SparkMapPartitionsWithInputSplit.scala
+++ SparkMapPartitionsWithInputSplitExample/src/main/scala/SparkMapPartitionsWithInputSplit.scala
@@ -1,35 +1,25 @@
 package com.github.tootop101
 
-import org.apache.spark.Logging
 import org.apache.spark.SparkContext
-import org.apache.spark.rdd.RDD
+import org.apache.spark.rdd.NewHadoopRDD
 import org.apache.spark.SparkConf
-import org.apache.spark.sql
-import org.apache.spark.sql._
-import org.apache.spark.sql.Row
-import org.apache.spark.sql.types._
-import org.apache.spark.sql.functions._
-import org.apache.spark.storage.StorageLevel
 
 import org.apache.hadoop.io.LongWritable
 import org.apache.hadoop.io.Text
-
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat
 import org.apache.hadoop.mapreduce.lib.input.FileSplit
-
-import org.apache.spark.rdd.NewHadoopRDD
+import org.apache.hadoop.fs.FileSystem
 
 object SparkMapPartitionsWithInputSplit {
   def main(args: Array[String]) {
 
     val debug = true
-    var inputDirectory: String = ""
-    var outputDirectory: String = ""
+    var inputDirectory = args(0)
+    var outputDirectory = args(1)
+
     val sparkConf = new SparkConf().setAppName("SparkMapPartitionsWithInputSplit")
-
     val sc = new SparkContext(sparkConf)
-
-	val hadoopConf = sc.hadoopConfiguration
+	  val hadoopConf = sc.hadoopConfiguration
 	
     val text = sc.newAPIHadoopFile(inputDirectory, classOf[TextInputFormat], classOf[LongWritable], classOf[Text], hadoopConf)
     val newHadoopRdd = text.asInstanceOf[NewHadoopRDD[LongWritable, Text]]
@@ -40,8 +30,8 @@ object SparkMapPartitionsWithInputSplit {
       val modifiyDatetimeLong = fs.getFileStatus(file.getPath).getModificationTime()
       val date = new java.util.Date(modifiyDatetimeLong)
       val dateFormat = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-      val modifiyDatetime = dateFormat.format(date)
-      iterator.map { x => (fileName, modifiyDatetime, x._2.toString()) }
+      val modifyDatetime = dateFormat.format(date)
+      iterator.map { x => (fileName, modifyDatetime, x._2.toString()) }
     }.cache()
   }
 
